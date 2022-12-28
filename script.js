1aa9bf7587177fa46adbf633c97504bdf2301b09
ILYAS ABDELLAOUI <ilyas.abdellaoui21@gmail.com>
2022-12-28 22:41:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+// Get the mode button and navbar elements
+const modeButton = document.querySelector(".mode-button");
+const nav = document.querySelector("nav");
+
+// Add an event listener to the mode button
+modeButton.addEventListener("click", function() {
+  // Toggle the "dark" class on the navbar element
+  nav.classList.toggle("dark");
+
+  // Update the text of the mode button
+  if (nav.classList.contains("dark")) {
+    modeButton.innerHTML = "Light Mode";
+  } else {
+    modeButton.innerHTML = "Dark Mode";
+  }
+});
+
+const navLinks = document.querySelectorAll("nav ul li a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("mouseenter", (event) => {
+    event.target.style.backgroundColor = "#00bcd4";
+  });
+  
+  link.addEventListener("mouseleave", (event) => {
+    event.target.style.backgroundColor = "";
+  });
+});
+
+// Check the width of the screen
+if (window.innerWidth < 768) {
+  // If the screen width is less than 768px, add a class to the navbar to make it responsive
+  document.querySelector("nav").classList.add("responsive");
+}
+
+// Add an event listener to the navbar button to toggle the responsive menu
+document.querySelector("nav button.navbar-button").addEventListener("click", function() {
+  this.classList.toggle("active");
+  document.querySelector("nav ul").classList.toggle("active");
+});
+
+
+// Get the navbar element
+var navbar = document.getElementById('navbar');
+
+// Add a class to the navbar for small screens
+if (window.innerWidth < 768) {
+  navbar.classList.add('small-screen');
+}
+
+// Get the body element
+let body = document.querySelector("body");
+
+// Create a new image element
+let img = new Image();
+
+// Set the source of the image
+img.src = "https://source.unsplash.com/1600x900/?nature";
+
+// When the image is loaded, set it as the background image of the body element
+img.addEventListener("load", function() {
+  body.style.backgroundImage = "url('" + img.src + "')";
+});
+
+
+// Add an event listener to the About Me button in the navbar
+document.querySelector("#about-me-button").addEventListener("click", function() {
+  // Redirect the user to the about page when the button is clicked
+  window.location = "about.html";
+});
+
+// Get all the cards on the page
+const cards = document.querySelectorAll('.card');
+
+// Add a shadow to each card when it is clicked
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.style.boxShadow = '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)';
+  });
+});
+
+
+const card = document.querySelector('.card'); // select the card element
+
+card.addEventListener('click', () => {
+  window.location.href = 'about.html'; // redirect to about page on click
+});
