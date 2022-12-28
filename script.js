774c41cd3ec1cc8d350c70ab58c01f48866602ef
ilyas0421 <ilyas.abdellaoui21@gmail.com>
2022-12-29 00:55:27 +0100

    modified:   nav.css 	modified:   script.js

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 // Get the navbar element
 const navbar = document.getElementById('navbar');
-// Get the mode button and navbar elements
-const modeButton = document.querySelector(".mode-button");
-const nav = document.querySelector("nav");
+
+// Get all the nav links that are not in the left group
 const navLinks = document.querySelectorAll("nav ul li a:not(.left-group a)");
 
-// Add a class to the navbar for small screens
-if (window.innerWidth < 768) {
-  navbar.classList.add('small-screen');
-}
-
-
-// Add an event listener to the navbar button to toggle the responsive menu
-document.querySelector("nav button.navbar-button").addEventListener("click", function() {
-  this.classList.toggle("active");
-  document.querySelector("nav ul").classList.toggle("active");
-});
 navLinks.forEach((link) => {
   link.addEventListener("mouseenter", (event) => {
     event.target.style.backgroundColor = "#00bcd4";
   });
   
   link.addEventListener("mouseleave", (event) => {
-    event.target.style.backgroundColor = "#00bcd4";
+    event.target.style.backgroundColor = "";
   });
+});
+
+// Add a class to the navbar for small screens
+if (window.innerWidth < 768) {
+  navbar.classList.add('small-screen');
+}
+
+// Add an event listener to the navbar button to toggle the responsive menu
+document.querySelector("nav button.navbar-button").addEventListener("click", function() {
+  this.classList.toggle("active");
+  document.querySelector("nav ul").classList.toggle("active");
 });
 
 // Get all the cards on the page
@@ -35,7 +34,6 @@ cards.forEach(card => {
     card.style.boxShadow = '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)';
   });
 });
-
 
 // Add an event listener to the About Me button in the navbar
 document.querySelector("#LA BOÎTE À MERVEILLES").addEventListener("click", function() {
@@ -54,3 +52,26 @@ img.addEventListener("load", function() {
   body.style.backgroundImage = "url('" + img.src + "')";
 });
 
+
+
+// Add an event listener to the About Me button in the navbar
+document.querySelector("#LA BOÎTE À MERVEILLES").addEventListener("click", function() {
+  // Redirect the user to the about page when the button is clicked
+  window.location = "page1.html";
+});
+
+// Get all the cards on the page
+
+// Add a shadow to each card when it is clicked
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.style.boxShadow = '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)';
+  });
+});
+
+
+const card = document.querySelector('.card'); // select the card element
+
+card.addEventListener('click', () => {
+  window.location.href = 'page1.html'; // redirect to about page on click
+});
