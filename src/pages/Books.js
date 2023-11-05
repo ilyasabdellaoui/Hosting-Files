2a9bf2b7d0bdcd1f6f909168459f3a932baa87f2
ilyasabdellaoui <ilyas.abdellaoui21@gmail.com>
2023-11-05 17:49:11 +0100

    Add Download Links

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -13,8 +13,8 @@ const BookData = [
       image: window.location + ".png",
       description: "Antigone, écrite par Jean Anouilh en 1944, est une réinterprétation moderne de la tragédie antique de Sophocle, mais elle est ancrée dans les dilemmes de la France occupée pendant la Seconde Guerre mondiale. La pièce raconte l'histoire d'une famille et de ses guerres internes touchant aux thèmes de la loyauté, de la justice et du divin. Antigone est une tragédie moderne qui met en lumière la lutte de la protagoniste, Antigone, pour défendre ses convictions morales et religieuses en dépit des lois édictées par le pouvoir établi. Le message central de la pièce est celui de la résistance individuelle face à l’autorité oppressante.",
       writer: "Jean Anouilh",
-      downloadLink: "https://www.google.com",
-      downloadFrench: "https://www.google.com"
+      downloadLink: "https://bit.ly/ANT_AR",
+      downloadFrench: "https://bit.ly/ANT_FR"
   },
   {
       abbr: "DJC",
@@ -22,8 +22,8 @@ const BookData = [
       image: window.location + ".png",
       description: "Le Dernier jour d'un condamné, écrit par Victor Hugo en 1829, est un roman qui raconte les dernières heures de vie d'un condamné à mort. Le roman est un réquisitoire contre la peine de mort et dénonce la cruauté de la justice. Le personnage principal, le condamné, est anonyme et représente tous les condamnés à mort. Le roman est divisé en 48 chapitres, et le dernier chapitre décrit les derniers moments du condamné avant son exécution. Le roman est écrit à la première personne, ce qui permet au lecteur de ressentir les émotions du condamné.",
       writer: "Victor Hugo",
-      downloadLink: "https://www.google.com",
-      downloadFrench: "https://www.google.com"
+      downloadLink: "https://bit.ly/DJC_AR",
+      downloadFrench: "https://bit.ly/DJC_FR"
   },
   {
       abbr: "BAM",
@@ -31,8 +31,8 @@ const BookData = [
       image: window.location + ".png",
       description: "La Boîte à merveilles est un roman autobiographique écrit par Ahmed Sefrioui en 1954. Le roman raconte la vie quotidienne d'une famille populaire dans la vieille ville de Fès, au Maroc, à l'époque du protectorat français. Le personnage principal, Sidi Mohammed, est un enfant de six ans qui décrit sa vie, ses rêves et ses peurs. Le roman est divisé en plus de trente souvenirs qui sont des tableaux de la vie quotidienne de la famille de Sidi Mohammed. La Boîte à merveilles est considérée comme un chef-d'œuvre de la littérature marocaine d'expression française.",
       writer: "Ahmed Sefrioui",
-      downloadLink: "https://www.google.com",
-      downloadFrench: "https://www.example.com"
+      downloadLink: "https://bit.ly/BAM_AR",
+      downloadFrench: "https://bit.ly/BAM_FR"
       
   }
 ];
